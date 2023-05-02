0f46447cd22049913ed74fba75ac996eb3a2d2f8
Divyanshu Singh <92361650+itsdivgithub@users.noreply.github.com>
2023-05-03 04:03:07 +0530

    Update streamlit_model_deployment.py

--- streamlit_model_deployment.py
+++ streamlit_model_deployment.py
@@ -1,30 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
 import streamlit as st
 import numpy as np
 from tensorflow.keras.models import load_model
 
+# Load the trained model
+model = load_model('my_model.h5')
 
-#Load the trained model
-model= load_model('ksp_model.h5')
-
+# Define a function to make a prediction
 def predict_target(gravity, ph, osmo, cond, urea, calc, osmo_cond_ratio, urea_calc_diff, osmo_urea_interaction, gravity_bin, ph_bin, osmo_bin, cond_bin, urea_bin, calc_bin):
-
+    
     # Create a numpy array with the input values and reshape it for the model
     input_data = np.array([[gravity, ph, osmo, cond, urea, calc, osmo_cond_ratio, urea_calc_diff, osmo_urea_interaction, gravity_bin, ph_bin, osmo_bin, cond_bin, urea_bin, calc_bin]])
     input_data = np.reshape(input_data, (input_data.shape[0], input_data.shape[1], 1))
-
-    #make prediction using the model
-
-    prediction= model.predict(input_data)
-
-    #Return the predicted target value
+    
+    # Make a prediction using the model
+    prediction = model.predict(input_data)
+    
+    # Return the predicted target value
     return prediction[0][0]
 
-#Add a title
-
-st.title('Kidney Stone Preiction App')
-
-#Add input field for the features
-
+    
+ # Add a title
+st.title("Kidney Stone Prediction App")
+    
+# Add input fields for the features
 gravity = st.number_input("Enter gravity value")
 ph = st.number_input("Enter pH value")
 osmo = st.number_input("Enter osmo value")
@@ -40,13 +44,19 @@ osmo_bin = st.number_input("Enter osmo_bin value")
 cond_bin = st.number_input("Enter cond_bin value")
 urea_bin = st.number_input("Enter urea_bin value")
 calc_bin = st.number_input("Enter calc_bin value")
-
-#Add a button to make predictions
-
-if st.button('Predict'):
+    
+# Add a button to make the prediction
+if st.button("Predict"):
     prediction = predict_target(gravity, ph, osmo, cond, urea, calc, osmo_cond_ratio, urea_calc_diff, osmo_urea_interaction, gravity_bin, ph_bin, osmo_bin, cond_bin, urea_bin, calc_bin)
     st.write("Predicted Target Value: ", prediction)
     if prediction > 0.3:
         st.write("You have kidney stone.")
     else:
-        st.write("You don't have kidney stone.")
+        st.write("You dont have kidney stone.")
+
+
+# In[ ]:
+
+
+
+
